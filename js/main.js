55304a59949a91ad7dbd8873b30dc37f009934b1
Diego <d.senkov29@gmail.com>
2020-12-10 12:13:33 +0200

    event listenters

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let textInput = document.getElementById('operationsInput')
+let clearButton = document.getElementById('clear')
+let backspaceButton = document.getElementById('backspace')
+let resultButton = document.getElementById('equalSign')
+
+clearButton.addEventListener('click', () => textInput.value = '')
+
+backspaceButton.addEventListener('click', () => {
+    let inputContent = textInput.value
+    textInput.value = inputContent.substring(0, inputContent.length-1)
+})
+
+let digitButtons = document.getElementsByClassName('calculator__digits-button')
+let operationButtons = 
+    [...document.getElementsByClassName('calculator__operations-button')]
+    .filter(button => !['clear', 'equalSign', 'backspace'].includes(button.id))
+
+let buttonsToFillInput = [...digitButtons, ...operationButtons]
+
+buttonsToFillInput.forEach(button => {
+    button.addEventListener('click', () => textInput.value += button.innerText)
+})
+
